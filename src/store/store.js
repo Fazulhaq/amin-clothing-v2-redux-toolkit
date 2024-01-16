59f4c7a0e1c50d53a88a133ac3d8a7813a118a97
Fazulhaq Amin <amin.jawan@yahoo.com>
2024-01-16 11:49:15 +0430

    user reducer configured from redux-toolkit

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import { persistStore, persistReducer } from 'redux-persist';
 // import storage from 'redux-persist/lib/storage';
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
-  Boolean
-);
+// const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+//   Boolean
+// );
 
 // const composeEnhancer =
 //   (process.env.NODE_ENV !== 'production' &&
@@ -28,7 +28,7 @@ const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: middleWares,
+  // middleware: middleWares,
 });
 
 // export const persistor = persistStore(store);
